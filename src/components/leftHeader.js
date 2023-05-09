a90bff48545ec943d9d0c61bbe9acdceeebd2706
alexanderscher <alexcscher@gmail.com>
2023-05-08 17:26:49 -0700

    setup

---
+++ src/components/leftHeader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+  // TODO: Add a style attribute to header and the h1 element
+
+  return <div>This is the left</div>;
+}
+
+export default Header;
