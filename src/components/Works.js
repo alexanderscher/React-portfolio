8248ce211ff736b9426325e9a3d4b19cc31971f8
alexanderscher <alexcscher@gmail.com>
2023-05-10 12:02:33 -0700

    added image

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,12 +1,10 @@
 import React from "react";
+import foody from "../img/foody.png";
+import weather from "../img/weather.png";
+import notetaker from "../img/notetaker.png";
+import src from "../img/src.png";
 
 const styles = {
-  right: {
-    backgroundColor: "white",
-    height: "100%",
-    width: "50%",
-    marginLeft: "50%",
-  },
   container: {
     width: "100%",
     display: "flex",
@@ -19,12 +17,18 @@ function Works() {
   return (
     <div className="right">
       <div style={styles.container}>
-        <div className="img-wrapper"></div>
-        <div className="img-wrapper"></div>
-        <div className="img-wrapper"></div>
-        <div className="img-wrapper"></div>
-        <div className="img-wrapper"></div>
-        <div className="img-wrapper"></div>
+        <div className="img-wrapper">
+          <img src={foody}></img>
+        </div>
+        <div className="img-wrapper">
+          <img src={weather}></img>
+        </div>
+        <div className="img-wrapper">
+          <img src={notetaker}></img>
+        </div>
+        <div className="img-wrapper">
+          <img src={src}></img>
+        </div>
       </div>
     </div>
   );
